c64dc86a9633562790db55c710aff3c049fc617e
sabrinaaraujoo <cien.sabrinaaraujo@outlook.com>
2023-01-04 19:12:08 -0300

    Adicionando dados com React

--- httpreact/src/App.js
+++ httpreact/src/App.js
@@ -6,6 +6,8 @@ const url = "http://localhost:3000/products";
 
 function App() {
   const [products, setProducts] = useState([]);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
 
   // Resgatando Dados
   useEffect(() => {
@@ -20,6 +22,24 @@ function App() {
     fetchData();
   }, []);
 
+  // 2 - add de produtos
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const product = {
+      name,
+      price,
+    };
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(product),
+    });
+  };
+
   return (
     <div className="App">
       <h1>Listas de Produtos</h1>
@@ -31,6 +51,29 @@ function App() {
           </li>
         ))}
       </ul>
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+          <label>
+            Nome:
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </label>
+          <label>
+            Preço:
+            <input
+              type="text"
+              name="price"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+            />
+          </label>
+          <input type="submit" value="Criar" />
+        </form>
+      </div>
     </div>
   );
 }
