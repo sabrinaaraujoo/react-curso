b6a1c245d248ab97ca295f3c108b05106c08551b
sabrinaaraujoo <cien.sabrinaaraujo@outlook.com>
2023-01-11 18:59:03 -0300

    Link ativo

--- react-router/src/components/Navbar.jsx
+++ react-router/src/components/Navbar.jsx
@@ -1,12 +1,15 @@
 // Links com React Router
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <nav>
-      <Link to="/">Home</Link>
-      <Link to="/about">Sobre</Link>
+      {/* <Link to="/">Home</Link>
+      <Link to="/about">Sobre</Link> */}
+      {/* Link Ativo */}
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/about">Sobre</NavLink>
     </nav>
   );
 };
