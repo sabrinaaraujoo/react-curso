af32134fae14d18dd07b0a89a9cd1ff1943f0cb7
sabrinaaraujoo <cien.sabrinaaraujo@outlook.com>
2023-01-15 18:10:59 -0300

    Refatorando contexto para hook

--- context-api/src/pages/About.jsx
+++ context-api/src/pages/About.jsx
@@ -1,8 +1,14 @@
-import { useContext } from "react";
-import { CounterContext } from "../context/CounterContext";
+// import { useContext } from "react";
+// import { CounterContext } from "../context/CounterContext";
+
+// Refatorando com Hook
+import { useCounterContext } from "../hooks/useCounterContext";
 
 const About = () => {
-  const { counter } = useContext(CounterContext);
+  // const { counter } = useContext(CounterContext);
+
+  const { counter } = useCounterContext();
+
   return (
     <div>
       <h1>About</h1>
--- context-api/src/pages/Home.jsx
+++ context-api/src/pages/Home.jsx
@@ -1,9 +1,14 @@
-import { useContext } from "react";
+// import { useContext } from "react";
 import ChangeCounter from "../components/ChangeCounter";
-import { CounterContext } from "../context/CounterContext";
+// import { CounterContext } from "../context/CounterContext";
+
+// Refatorando com Hook
+import { useCounterContext } from "../hooks/useCounterContext";
 
 const Home = () => {
-  const { counter } = useContext(CounterContext);
+  // const { counter } = useContext(CounterContext);
+
+  const { counter } = useCounterContext();
 
   return (
     <div>
